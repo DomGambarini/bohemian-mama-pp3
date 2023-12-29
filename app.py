60eca530befb0017cf1f33a22a3d687004d791bd
DomGambarini <>
2023-12-29 13:54:23 +0000

    Add login functionality

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import (
     session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash, check_password_hash 
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,17 +22,6 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
-@app.route("/seasonalrecipes.html")
-def seasonalrecipes():
-    recipes = mongo.db.recipes.find()
-    return render_template("seasonalrecipes.html", recipes=recipes)
-
-
-@app.route("/login.html")
-def login():
-    return render_template("login.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -56,6 +45,41 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
     return render_template("register.html")
+
+
+@app.route("/signin", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@app.route("/seasonalrecipes.html")
+def seasonalrecipes():
+    recipes = mongo.db.recipes.find()
+    return render_template("seasonalrecipes.html", recipes=recipes)
 
 
 if __name__ == "__main__":
