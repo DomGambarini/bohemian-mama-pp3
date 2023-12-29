ecbdf752d95d98d51b4fddca8ffd93d40eb09254
DomGambarini <>
2023-12-29 14:17:17 +0000

    Add profile functionality

--- app.py
+++ app.py
@@ -44,6 +44,8 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
@@ -74,6 +76,13 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab session user's username from mongoDB
+    username = mongo.db.users.find_one({"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 @app.route("/seasonalrecipes.html")
