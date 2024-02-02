be8d51b327cb9d2586d9d0839597d87656037a92
DomGambarini <>
2024-02-02 18:07:16 +0000

    Server issue when rendering page in heroku. Restored back to original form app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import os
-import form
 from flask import (
     Flask, flash, render_template,
     redirect, request,
     session, url_for)
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms.validators import (
+    InputRequired, Length, NumberRange, URL, DataRequired)
+from wtforms import (
+    StringField, PasswordField, RadioField, IntegerField,
+    SubmitField, SelectField, TextAreaField, DateField, URLField)
 from flask_pymongo import PyMongo
 from datetime import datetime
 from bson.objectid import ObjectId
@@ -24,6 +29,35 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 current_time = datetime.now()
 formatted_date = current_time.strftime("%d/%m/%Y")
+
+
+class RecipeForm(FlaskForm):
+    season_name = SelectField(
+        u'Select a Season',
+        choices=[
+            ('', 'Choose your option'),
+            ('Winter', 'Winter'),
+            ('Spring', 'Spring'),
+            ('Summer', 'Summer'),
+            ('Autumn', 'Autumn')],
+        validators=[InputRequired()],
+        default=''
+    )
+    recipe_name = StringField(
+        'Recipe Name', validators=[InputRequired(), Length(min=4, max=40)])
+    cook_time = IntegerField(
+        'Cook/Prep Time in Minutes', validators=[InputRequired(), NumberRange(
+            min=1, max=300, message="Please enter a number between 1 and 300."
+        )])
+    serves = IntegerField(
+        'How Many Does it Serve', validators=[
+            InputRequired(), NumberRange(min=1, max=20)])
+    ingredients = TextAreaField('Ingredients', validators=[
+        InputRequired(), Length(min=30, max=1000)])
+    method = TextAreaField('Method', validators=[
+        InputRequired(), Length(min=30, max=5000)])
+    image = URLField('Add URL Image Here', validators=[InputRequired()])
+    submit = SubmitField('Add Recipe')
 
 
 @app.route("/")
