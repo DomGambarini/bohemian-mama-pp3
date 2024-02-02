648f65fbfc77b7763fc310b80d2befbdbd137158
DomGambarini <>
2024-02-02 17:38:55 +0000

    Imported form.py into app.py and vice versa

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import form.py
 from flask import (
     Flask, flash, render_template,
     redirect, request,
@@ -29,35 +30,6 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 current_time = datetime.now()
 formatted_date = current_time.strftime("%d/%m/%Y")
-
-
-class addRecipe(FlaskForm):
-    season_name = SelectField(
-        u'Select a Season',
-        choices=[
-            ('', 'Choose your option'),
-            ('Winter', 'Winter'),
-            ('Spring', 'Spring'),
-            ('Summer', 'Summer'),
-            ('Autumn', 'Autumn')],
-        validators=[InputRequired()],
-        default=''
-    )
-    recipe_name = StringField(
-        'Recipe Name', validators=[InputRequired(), Length(min=4, max=40)])
-    cook_time = IntegerField(
-        'Cook/Prep Time in Minutes', validators=[InputRequired(), NumberRange(
-            min=1, max=300, message="Please enter a number between 1 and 300."
-        )])
-    serves = IntegerField(
-        'How Many Does it Serve', validators=[
-            InputRequired(), NumberRange(min=1, max=20)])
-    ingredients = TextAreaField('Ingredients', validators=[
-        InputRequired(), Length(min=30, max=1000)])
-    method = TextAreaField('Method', validators=[
-        InputRequired(), Length(min=30, max=5000)])
-    image = URLField('Add URL Image Here', validators=[InputRequired()])
-    submit = SubmitField('Add Recipe')
 
 
 @app.route("/")
---
+++ form.py
@@ -0,0 +1,30 @@
+import app.py
+
+
+class addRecipe(FlaskForm):
+    season_name = SelectField(
+        u'Select a Season',
+        choices=[
+            ('', 'Choose your option'),
+            ('Winter', 'Winter'),
+            ('Spring', 'Spring'),
+            ('Summer', 'Summer'),
+            ('Autumn', 'Autumn')],
+        validators=[InputRequired()],
+        default=''
+    )
+    recipe_name = StringField(
+        'Recipe Name', validators=[InputRequired(), Length(min=4, max=40)])
+    cook_time = IntegerField(
+        'Cook/Prep Time in Minutes', validators=[InputRequired(), NumberRange(
+            min=1, max=300, message="Please enter a number between 1 and 300."
+        )])
+    serves = IntegerField(
+        'How Many Does it Serve', validators=[
+            InputRequired(), NumberRange(min=1, max=20)])
+    ingredients = TextAreaField('Ingredients', validators=[
+        InputRequired(), Length(min=30, max=1000)])
+    method = TextAreaField('Method', validators=[
+        InputRequired(), Length(min=30, max=5000)])
+    image = URLField('Add URL Image Here', validators=[InputRequired()])
+    submit = SubmitField('Add Recipe')
